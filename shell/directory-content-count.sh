f33319c56f460b78d3d665446148bb2bb7c6567f
Harry Wright <hcawright@gmail.com>
2020-03-01 20:56:45 +0000

    Update directory-content-count.sh

--- shell/directory-content-count.sh
+++ shell/directory-content-count.sh
@@ -4,6 +4,11 @@
 
 # Lists the count of files inside each directory, useful for when uploads are taking ages for dropbox etc.
 
-# One-liner
+find . -maxdepth 1 -type d -print0 | while read -d '' -r dir; do
+    num=$(find "$dir" -ls | wc -l)
+    printf "%5d files in directory %s\n" "$num" "$dir"
+done
+
+# As a one-liner
 
 find . -maxdepth 1 -type d -print0 | while read -d '' -r dir; do num=$(find "$dir" -ls | wc -l); printf "%5d files in directory %s\n" "$num" "$dir"; done
